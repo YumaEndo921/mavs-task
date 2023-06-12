17cfed60f5292769c089e2369f62ced57e8c0dcc
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-06-12 16:55:14 +0900

    6/12
    fix

--- frontend/src/pages/add.vue
+++ frontend/src/pages/add.vue
@@ -1,47 +1,47 @@
 <template>
-    <div class="add">
-        <h2 class="add__ttl">Memo Add</h2>
-        <form class="add__form" @submit.prevent="submit">
-            <InputTitle  v-model="formValue.title" />
-            <InputContent  v-model="formValue.content" />
-            <InputButton buttonName="保存" />
-        </form>
-        <div class="add__box">
-        <nuxt-link class="add__back"  to="/">戻る</nuxt-link>
-        </div>
+  <div class="add">
+    <h2 class="add__ttl">Memo Add</h2>
+    <form class="add__form" @submit.prevent="submit">
+      <InputTitle v-model="formValue.title" />
+      <InputContent v-model="formValue.content" />
+      <InputButton buttonName="保存" />
+    </form>
+    <div class="add__box">
+      <nuxt-link class="add__back" to="/">戻る</nuxt-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
+  data() {
     return {
       formValue: {
         title: '',
         content: '',
-        userId:'',
+        userId: '',
       },
     }
   },
-    computed:{
-        // トークンをstoreから取得
-        getToken () {
-            return this.$store.state.auth.token
-        },
-        // ログイン中のユーザーidをstoreから取得
-        getUserId(){
-            return this.$store.state.auth.id
-        },
+  computed: {
+    // トークンをstoreから取得
+    getToken() {
+      return this.$store.state.auth.token
     },
-    methods:{
-      async submit() {
+    // ログイン中のユーザーidをstoreから取得
+    getUserId() {
+      return this.$store.state.auth.id
+    },
+  },
+  methods: {
+    async submit() {
       //タイトルの入力確認
-      if(this.formValue.title === ''){
-          this.$toast.global.error({
+      if (this.formValue.title === '') {
+        this.$toast.global.error({
           message: 'タイトルを入力してください。',
         })
         return
-        }
+      }
 
       //ログイン中のuserIdをformValue格納
       this.formValue.userId = this.getUserId
@@ -49,9 +49,10 @@ export default {
       const response = await this.$axios.post(
         `${this.$config.apiBaseUrl}/articles/add`,
         //入力された情報とユーザーidを渡す
-        {params:this.formValue},
+        { params: this.formValue },
         //ヘッダーリクエストにトークン認証を渡す
-        { headers: {Authorization:this.getToken} })
+        { headers: { Authorization: this.getToken } }
+      )
       //バックエンドからのresを格納
       const responseCode = response.data.body
       if (responseCode === 200) {
@@ -66,43 +67,41 @@ export default {
         })
       }
     },
-    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.add{
-    @media screen and (max-width: 480px) {
-      background-color: black;
-    }
+.add {
+  @media screen and (max-width: 480px) {
+    background-color: black;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &__ttl {
+    margin-bottom: 62px;
+    text-align: center;
+    font-size: 64px;
+  }
+  &__form {
+    // width: 85%;
     display: flex;
     flex-direction: column;
+    align-items: flex-start;
+  }
+  &__input {
+    margin-bottom: 62px;
+  }
+  &__back {
+    display: flex;
+    justify-content: center;
     align-items: center;
-    &__ttl{
-        margin-bottom: 62px;
-        text-align: center;
-        font-size: 64px;
-    }
-    &__form{
-        // width: 85%;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-    }
-    &__input{
-        margin-bottom: 62px;
-    }
-    &__back{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 144px;
-        height: 60px;
-        margin-bottom: 350px;
-        background: #D9D9D9;
-        border-radius: 5px;
-    }
+    width: 144px;
+    height: 60px;
+    margin-bottom: 350px;
+    background: #d9d9d9;
+    border-radius: 5px;
+  }
 }
-
-
 </style>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,35 +1,48 @@
 <template>
   <div>
-
     <!-- ログイン中 -->
-      <div v-if="isLogin" class="indexLogin">
-        <h2 class="indexLogin__ttl">Memo List</h2>
-        <div class="indexLogin__box">
-          <nuxt-link class="indexLogin__box--button" to="/add">新規作成</nuxt-link>
-        </div>
-
-        <!-- ユーザーメモリスト -->
-        <ul class="indexLogin__table">
-          <li class="indexLogin__table--list"  v-for="(item , index ) in items" :key="item.id">
-            <!-- インデックス番号(配列番号+1) -->
-            <p>{{ index + 1 }}</p>
-            <!-- タイトルの表示 -->
-            <button class="indexLogin__table--ttl" @click="setEdit(index)">{{ item.title }}</button>
-            <!-- 削除ボタン -->
-            <button class="indexLogin__table--del" @click="deleteMemo(item.id,index)">削除</button>
-          </li>
-        </ul>
+    <div v-if="isLogin" class="indexLogin">
+      <h2 class="indexLogin__ttl">Memo List</h2>
+      <div class="indexLogin__box">
+        <nuxt-link class="indexLogin__box--button" to="/add"
+          >新規作成</nuxt-link
+        >
       </div>
+
+      <!-- ユーザーメモリスト -->
+      <ul class="indexLogin__table">
+        <li
+          class="indexLogin__table--list"
+          v-for="(item, index) in items"
+          :key="item.id"
+        >
+          <!-- インデックス番号(配列番号+1) -->
+          <p>{{ index + 1 }}</p>
+          <!-- タイトルの表示 -->
+          <button class="indexLogin__table--ttl" @click="setEdit(index)">
+            {{ item.title }}
+          </button>
+          <!-- 削除ボタン -->
+          <button
+            class="indexLogin__table--del"
+            @click="deleteMemo(item.id, index)"
+          >
+            削除
+          </button>
+        </li>
+      </ul>
+    </div>
 
     <!-- ログイン前 -->
-      <div v-else class="index">
-        <h2 class="index__ttl">Memo App</h2>
-        <div class="index__box">
-          <nuxt-link class="index__box--button" to="/signin">サインイン</nuxt-link>
-          <nuxt-link class="index__box--button" to="/signup">新規登録</nuxt-link>
-        </div>
+    <div v-else class="index">
+      <h2 class="index__ttl">Memo App</h2>
+      <div class="index__box">
+        <nuxt-link class="index__box--button" to="/signin"
+          >サインイン</nuxt-link
+        >
+        <nuxt-link class="index__box--button" to="/signup">新規登録</nuxt-link>
       </div>
-
+    </div>
   </div>
 </template>
 
@@ -43,138 +56,138 @@ export default {
       return !!this.$store.state.auth.token
     },
     // ログイン中のユーザーidをstoreから取得
-    getUserId(){
-    return this.$store.state.auth.id
-   },
-   // storeのユーザーメモ情報を変数化（配列）
+    getUserId() {
+      return this.$store.state.auth.id
+    },
+    // storeのユーザーメモ情報を変数化（配列）
     items() {
-    return this.$store.state.auth.memo
-  }
+      return this.$store.state.auth.memo
+    },
   },
-  methods:{
+  methods: {
     //クリックされたボタンのメモを削除
-      async deleteMemo(item_id,index){
+    async deleteMemo(item_id, index) {
       //APIリクエストをバックエンド側へ送信
       const response = await this.$axios.$delete(
-      `${this.$config.apiBaseUrl}/articles/delete`,
-      //クリックされた行のメモidを渡す
-      { data: {
-        id:item_id}})
+        `${this.$config.apiBaseUrl}/articles/delete`,
+        //クリックされた行のメモidを渡す
+        {
+          data: {
+            id: item_id,
+          },
+        }
+      )
       //storeのメモ情報を削除
       await this.$store.commit('auth/deleteUserMemo', index)
     },
     //クリックされたメモのidをstoreに保存
-      async setEdit(index){
+    async setEdit(index) {
       await this.$store.commit('auth/setEdit', index)
       //editページへ遷移
       this.$router.push('/edit')
     },
   },
   //ユーザーメモ情報の取得と保存
-    async fetch() {
-      try {
+  async fetch() {
+    try {
       //APIリクエストをバックエンド側へ送信
       const response = await this.$axios.$post(
-      `${this.$config.apiBaseUrl}/articles/get`,
-      //userのidを渡す
-      { params: this.getUserId })
+        `${this.$config.apiBaseUrl}/articles/get`,
+        //userのidを渡す
+        { params: this.getUserId }
+      )
 
       await this.$store.commit('auth/setUserMemo', response.body)
-    } 
-    //メモ情報がなければ処理を終了
-    catch (error) {
+    } catch (error) {
+      //メモ情報がなければ処理を終了
       return
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 //ログイン前のcss
-.index{
-  &__ttl{
+.index {
+  &__ttl {
     padding-bottom: 133px;
     text-align: center;
     font-size: 64px;
   }
-  &__box{
+  &__box {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap:50px;
+    gap: 50px;
     text-align: center;
-    &--button{
+    &--button {
       width: 240px;
       height: 60px;
       display: flex;
       justify-content: center;
       align-items: center;
-      background: #D9D9D9;
+      background: #d9d9d9;
     }
   }
 }
 //ログイン後のcss
-.indexLogin{
-  
-  &__ttl{
+.indexLogin {
+  &__ttl {
     padding-bottom: 62px;
     text-align: center;
     font-size: 64px;
   }
-  &__box{
+  &__box {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap:50px;
+    gap: 50px;
     padding-bottom: 80px;
     text-align: center;
-    &--button{
+    &--button {
       width: 240px;
       height: 60px;
       display: flex;
       justify-content: center;
       align-items: center;
-      background: #D9D9D9;
+      background: #d9d9d9;
     }
   }
-  &__table{
+  &__table {
     text-align: center;
-    &--list{
+    &--list {
       display: flex;
       justify-content: center;
       align-items: center;
       gap: 64px;
       margin-bottom: 15px;
     }
-    &--ttl{
+    &--ttl {
       display: flex;
       justify-content: flex-start;
       align-items: center;
       width: 653px;
       height: 70px;
       padding-left: 10px;
-      background: #D9D9D9;
+      background: #d9d9d9;
       border: none;
     }
-    &--link{
+    &--link {
       display: block;
       background: #000;
     }
-    &--del{
+    &--del {
       display: flex;
       justify-content: center;
       align-items: center;
       width: 123px;
       height: 66px;
-      background: #8E8E8E;
+      background: #8e8e8e;
       border: none;
-      border-radius:5px;
+      border-radius: 5px;
     }
   }
 }
-
 </style>
-
-
